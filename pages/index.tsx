5bec0030ffa7ec137f6857724e2e58c531555d60
Dirk Stahlecker <dirkstahlecker@Dirks-MacBook-Pro.local>
2020-04-18 19:54:36 -0400

    added typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,16 +12,6 @@ export default function Home() {
         <h1 className="title">
           Hello World!
         </h1>
-
-          <a
-            href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with ZEIT Now.
-            </p>
-          </a>
       </main>
 
       <footer>
